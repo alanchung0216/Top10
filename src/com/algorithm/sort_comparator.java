1a94e16ad9de59b1b9d2f329b4df26f3aac380b7
alanc <alanc@localhost>
2016-06-18 15:47:06 -0400

    commit

---
+++ src/com/algorithm/sort_comparator.java
@@ -0,0 +1,36 @@
+package com.algorithm;
+
+/*
+Write a Comparator in Java to compare two employees based upon 
+there name, departments and age?
+This is pure Java based Coding exercise. In order to solve this 
+Java coding or programming interview question you need to know 
+What is comparator in Java and How to use compare method in Java 
+for sorting Object. Sorting is one of the most logical and 
+practical question on technical interview and ability to sort 
+Java object is must to code in Java.  
+
+This article 
+	http://java67.blogspot.com/2012/10/
+	how-to-sort-object-in-java-comparator-comparable-example.html
+
+help you to solve this Java coding question by 
+explaining how to sort object in Java using Comparable and Comparator. 
+Just remember that Comparable has compareTo() method and use to 
+sort object based upon there natural order e.g. numeric order 
+for number, and alphabetic order for String, while Comparator 
+can define any arbitrary sorting. A good follow up question can 
+also be difference between Comparator and Comparable in Java, 
+so be ready for that.
+
+
+Read more: http://java67.blogspot.com/2012/08/10-java-coding-interview-questions-and.html#ixzz4BxhGhKWK
+ */
+public class sort_comparator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
