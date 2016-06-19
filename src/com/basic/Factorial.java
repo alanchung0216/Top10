471e9d357ada97cea5c2c4df32ccd3da1fd0a323
alanc <alanc@localhost>
2016-06-19 16:52:14 -0400

    c

---
+++ src/com/basic/Factorial.java
@@ -0,0 +1,37 @@
+package com.basic;
+/*
+Factorials are very simple things. They're just products, 
+indicated by an exclamation mark. For instance, "four factorial" 
+is written as "4!" and means 1×2×3×4 = 24. 
+In general, n! ("enn factorial") means the product of all the 
+whole numbers from 1 to n; that is, n! = 1×2×3×...×n.
+ */
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static int fact(int n){
+		int total=1;
+		for (int i=1; i<=n; i++) {
+			total *= i;
+		}
+		return total;
+	}
+	
+	public static int fact_recur(int n) {
+		if (n == 1) return 1;
+		return fact_recur(n-1)*n;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		while (sc.hasNextLine()) {
+			int num = sc.nextInt();
+			System.out.println(num + "! factorial is " + fact(num));
+			System.out.println(num + "! factorial recur is " + fact_recur(num));
+		}
+	}
+
+}
---
+++ src/com/basic/PrimeNumber.java
@@ -0,0 +1,49 @@
+package com.basic;
+
+/*
+Write a Java program to find if a number is prime number or not
+One more basic Java program, which made it's way to Interviews. 
+One of the simplest coding question and also a very popular 
+Java programming exercise. Beauty of these kinds of logical 
+questions is that, they can really test basic programming skills 
+or a coder, programmer or developer. Not just problem solving, 
+you can also check there coding style and thought process. 
+By the way. you can  check this article for answer of this Java 
+coding interview question.
+http://javarevisited.blogspot.com/
+2012/04/java-program-to-print-prime-numbers-in.html
+
+
+Read more: http://java67.blogspot.com/2012/08/10-java-coding-interview-questions-and.html#ixzz4C3cJ04rh
+ */
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	//A number is called prime number if its not divisible by 
+	//any number other than 1 or itself
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long num;
+		System.out.println("enter num: ");
+		while (sc.hasNextLine()){
+			num=sc.nextLong();
+			boolean prime=true;
+			for (long i=2; i<num; i++){
+				if ((num % i) == 0) {
+					System.out.println(num + " it's not prime dividible by " + i);
+					prime=false;
+					break;
+				}
+			}
+			if (prime) System.out.println(num + " it's a prime");
+			
+			System.out.println("enter num: ");
+		}
+		sc.close();
+	}
+
+}
