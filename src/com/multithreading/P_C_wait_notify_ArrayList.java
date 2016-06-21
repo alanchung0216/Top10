61c17bdb1f625e06694a4d43ed715ff13a6b5ed0
alanc <alanc@192.168.1.67>
2016-06-21 13:59:35 -0400

    c

---
+++ src/com/multithreading/P_C_wait_notify_ArrayList.java
@@ -0,0 +1,94 @@
+package com.multithreading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class producer implements Runnable {
+	private final List<Integer> sharedQueue;
+	private final int SIZE;
+	producer(List<Integer> l, int sz){
+		sharedQueue = l;
+		SIZE = sz;
+	}
+	public void run() {
+		// add data into queue
+		for (int i=0; i<8; i++){
+			while (sharedQueue.size() == SIZE) {
+				synchronized(sharedQueue){
+					System.out.println(" producer waiting");
+					try {
+						sharedQueue.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}				
+				}
+			}
+			synchronized(sharedQueue){
+				sharedQueue.add(i);
+				System.out.println("add "+i
+				+" and size is "+sharedQueue.size());
+				sharedQueue.notifyAll();
+			}
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
+	}
+}
+
+class consumer implements Runnable {
+	
+	private final List<Integer> sharedQueue;
+	
+	consumer(List<Integer> l){
+		sharedQueue = l;
+	}
+	public void run() {
+		while (true){
+			while (sharedQueue.isEmpty()) {
+				synchronized(sharedQueue){
+					System.out.println(" consumer waiting");
+					try {
+						sharedQueue.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			synchronized(sharedQueue){
+				//int rem = sharedQueue.get(0);
+				sharedQueue.notifyAll();
+				sharedQueue.remove(0);
+				System.out.println("remove and size is "
+						+sharedQueue.size());
+			}
+			/*
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+		}
+	}
+}
+
+public class P_C_wait_notify_ArrayList {
+
+	public static void main(String[] args) {
+
+		// create a queue
+		List<Integer> queue = new ArrayList<Integer>();
+		Thread tp = new Thread(new producer(queue, 4));
+		Thread tc = new Thread(new consumer(queue));
+		tp.start(); // this one first works  if it sleep a little
+		tc.start();
+
+	}
+}
