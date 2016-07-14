d6d52d02d4c997520f3c4e964fced9a799967272
alanchung <alanchung@alans-imac.home>
2016-07-14 09:53:51 -0400

    c

--- src/com/string/int_to_hex_str.java
+++ src/com/string/int_to_hex_str.java
@@ -37,22 +37,23 @@ public class int_to_hex_str {
 		//int q=0;
 		int r=0;
 		List<Character> lc = new ArrayList<Character>();
-		StringBuilder sb = new StringBuilder();
+		//StringBuilder sb = new StringBuilder();
 		//while (num / 16 != 0){
 		while (num != 0){
 			r = num % 16;
 			num = num / 16;
 			//num = q;
 			lc.add(toHex(r));
-			sb.append(toHex(r));
+			//sb.append(toHex(r));
+			
 		}
 		//lc.add(toHex(q));
 		char[] ca= new char[lc.size()];
 		for (int i=0, j=lc.size()-1; i<lc.size(); i++, j--){
 			ca[j] = lc.get(i);
 		}
-		return sb.toString();
-		//return new String(ca);
+		// return sb.toString(); // need to reverse it 
+		return new String(ca);
 	}
 	public static void main(String[] args) {
 		String str=convert(43);
