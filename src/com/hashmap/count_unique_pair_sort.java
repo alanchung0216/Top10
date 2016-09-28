7e7ea1954560726b36f82c369176c008a7214e69
alanchung <alanchung@alans-imac.home>
2016-09-28 16:56:38 -0400

    c

--- src/com/hashmap/count_unique_pair_sort.java
+++ src/com/hashmap/count_unique_pair_sort.java
@@ -18,52 +18,16 @@ public class count_unique_pair_sort {
 
 
 	public static void main(String[] args) {
-		/*
-		int[] ia = {1, 4, 7, 40, 7, 46, 32};
-		int k = 47;
-		Arrays.sort(ia);
-		for (int i=0; i<ia.length; i++){
-			System.out.println(ia[i]);
-		}	
-		*/	
-		// eliminate duplicates
-		/*
-		int[] ib = new int[ia.length];
-		ib[0] = ia[0];
-		int pre = ia[0];
-		for (int i=1, j=1; i<ia.length; i++, j++){
-			if (ia[i] == pre) {
-				j--;
-				continue;
-			} else{
-				ib[j] = ia[i];
-				pre = ia[i];
-			}
-		}
-		System.out.println();
-		for (int i=0; i<ib.length; i++){
-			System.out.println(ib[i]);
-		}
-		// find non zero count
-		int count = 0;
-		for (int i=0; i<ib.length ; i++)
-			if (ib[i] != 0) count++;
-		int[] ic = new int[count];
-		for (int i=0; i<ic.length; i++) {
-			ic[i] = ib[i];
-		}
-		System.out.println();
-		for (int i=0; i<ic.length; i++){
-			System.out.println(ic[i]);
-		}	
-		*/
-		// or use HashSet to remove duplicates
-		System.out.println("use HashSet ");
+		
+//  use HashSet to remove duplicates
+
 		int[] id = {1, 4, 7, 40, 7, 46, 32};
+		int k = 47;
 		Set<Integer> hs = new HashSet<Integer>();
 		for (Integer i:id){
 			hs.add(i);
 		}
+		// copy set to ik array
 		int[] ik = new int[hs.size()];
 		Iterator<Integer> iter = hs.iterator();
 		int m=0;
@@ -71,13 +35,19 @@ public class count_unique_pair_sort {
 			ik[m] = iter.next();
 			m++;
 		}
-		Arrays.sort(ia);
-		System.out.printf("m ", m);
-		for (int i=0; i<ik.length; i++){
-			System.out.printf("%d ", ik[i]);
-		}		
-		//for (int i=0; i<)
-		// use binary search to calculate diff count
+		// sort ik
+		Arrays.sort(ik);
+
+		int count=0;
+		for (int i=0; i<ik.length; i++) {
+			for (int j=i+1; j<ik.length; j++){
+				if ((ik[i] + ik[j]) == k){
+					count++;
+				}				
+			}
+		}
+		System.out.println("count " + count);
+
 	}
 
 }
